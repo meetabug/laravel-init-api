875bef14921b110653e5375a21e167ca2966c448
meetabug <1272526724@qq.com>
2019-03-29 15:03:35 +0800

    unified status code

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\UserRequest;
+use App\Http\Resources\Api\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
@@ -13,26 +13,26 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::paginate(3);
-        return $users;
+        return UserResource::collection($users);
     }
 
     public function show(User $user)
     {
-        return $user;
+        return $this->success(new UserResource($user));
     }
     
     public function store(UserRequest $request)
     {
         User::create($request->all());
-        return '用户注册成功。。。';
+        return $this->setStatusCode(201)->success('用户注册成功');
     }
 
     public function login(Request $request)
     {
         $res = Auth::guard('web')->attempt(['name'=>$request->name,'password'=>$request->password]);
         if($res){
-            return '用户登录成功...';
+            return $this->setStatusCode(201)->success('用户登录成功...');
         }
-        return '用户登录失败';
+        return $this->failed('用户登录失败',401);
     }
 }

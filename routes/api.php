a8c8e58d6cbc4e4351280ea9915a8660ee658843
meetabug <1272526724@qq.com>
2019-03-29 14:49:43 +0800

    install medz/cors and init config

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::namespace('Api')->prefix('v1')->group(function () {
+Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
     Route::get('/users', 'UsersController@index')
         ->name('users.index');
     Route::get('/users/{user}', 'UsersController@show')
